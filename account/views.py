6ffe04bb5f4f137b21b2b7e7f9225f26d7ff6e21
marik177 <marik177@gmail.com>
2023-03-22 11:48:10 +0300

    Added LoginView and LogoutView

--- account/views.py
+++ account/views.py
@@ -1,8 +1,7 @@
-from django.contrib.auth.views import LoginView
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
-from django.views import View
+from django.contrib.auth.decorators import login_required
 
 from .forms import LoginForm
 
@@ -28,3 +27,8 @@ def user_login(request):
     return render(request, 'account/login.html', {'form': form})
 
 
+@login_required
+def dashboard(request):
+    return render(request,
+                  'account/dashboard.html',
+                  {'section': 'dashboard'})
