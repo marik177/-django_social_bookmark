e6d5f1327cb01898d779268e9b7aec6341176bba
marik177 <marik177@gmail.com>
2023-03-23 12:06:55 +0300

    Added registration form

--- account/views.py
+++ account/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
-from .forms import LoginForm
+from .forms import LoginForm, UserRegistrationForm
 
 
 def user_login(request):
@@ -32,3 +32,22 @@ def dashboard(request):
     return render(request,
                   'account/dashboard.html',
                   {'section': 'dashboard'})
+
+
+def register(request):
+    if request.method == 'POST':
+        user_form = UserRegistrationForm(request.POST)
+        if user_form.is_valid():
+            # Create a new User object
+            new_user = user_form.save(commit=False)
+            # Set the choosen password
+            new_user.set_password(
+                user_form.cleaned_data['password'])
+            # Save the User object
+            new_user.save()
+        return render(request,
+                      'account/register_done.html', {'new_user': new_user})
+    else:
+        form = UserRegistrationForm()
+        return render(request,
+                      'account/register.html', {'form': form})
