11584c56113bc80af2248879a2742b046da6efae
marik177 <marik177@gmail.com>
2023-03-28 13:19:15 +0300

    created new app images with new model Image

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'mysite.com']
 
 INSTALLED_APPS = [
     "account.apps.AccountConfig",
+    "images.apps.ImagesConfig",
 
     "django.contrib.admin",
     "django.contrib.auth",
---
+++ images/views.py
@@ -0,0 +1,21 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from .forms import ImageCreateForm
+
+
+@login_required
+def image_create(request):
+    if request.method == 'POST':
+        form = ImageCreateForm(request.POST)
+        if form.is_valid():
+            new_image = form.save(commit=False)
+            new_image.user = request.user
+            new_image.save()
+            messages.success(request,
+                             'Image added successfully.')
+            return redirect(new_image.get_absolute_url())
+    else:
+        form = ImageCreateForm(request.GET)
+    return render(request, 'images/image/create.html',
+                  {'form': form, 'section': 'images'})
